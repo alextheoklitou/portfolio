60e6d4937d129e82474715ca3fb86d8eb94ac86d
Alex Theoklitou <alex_theo_cy@hotmail.com>
2022-07-26 18:25:09 +0100

    readme

---
+++ src/assets/projects/projectData.js
@@ -0,0 +1,34 @@
+export const ProjectData = [
+  {
+    name: 'PawHub',
+    imageUrl: 'https://i.imgur.com/eBxpblB.png?1',
+    url: 'https://paw-hub.netlify.app/',
+    readMeUrl: 'https://github.com/alextheoklitou/sei-project-four-fe',
+    alt: 'pawhub',
+    info: 'A full-stack app created with Python, Django, React.js & Tailwind. A clone of the website the Dogs Trust, this app will allow you to browse, favourite and leave comments on the dogs you like.'
+  },
+  {
+    name: 'Sniff',
+    imageUrl: 'https://i.imgur.com/FfN0XQ4.png?1',
+    url: 'https://sniff.netlify.app/',
+    readMeUrl: 'https://github.com/alextheoklitou/sei-project-3-fe',
+    alt: 'sniff',
+    info: 'A full-stack MERN app - MongoDB, Express.js, React.js & Node.js with CRUD functionality. Just what 2022 needs - a dating app for animals. Here you can sign up, sign in & initiate chats with other animals on the app.'
+  },
+  {
+    name: 'Mystic Ally\'s Tarot Reading',
+    imageUrl: 'https://i.imgur.com/jtnJ1le.png?1',
+    url: 'https://mystic-allys-tarot-reading.netlify.app/',
+    readMeUrl: 'https://github.com/alextheoklitou/sei-project-two',
+    alt: 'mystic ally\'s tarot reading',
+    info: 'A hackathon style challenge built with React.js and an existing API. This app provides the user an opportunity to receive a tarot card reading.'
+  },
+  {
+    name: 'Space Invaders',
+    imageUrl: 'https://i.imgur.com/X2QwQxE.png?1',
+    url: 'https://alextheoklitou.github.io/sei-project-one/',
+    readMeUrl: 'https://github.com/alextheoklitou/sei-project-one',
+    alt: 'space invaders',
+    info: 'A grid based game built using vanilla JavaScript, HTML & CSS based on the original arcade game with a Lego theme. This was my first project built from scratch as part of the General Assembly Software Engineering Immersive Course.'
+  },
+]
